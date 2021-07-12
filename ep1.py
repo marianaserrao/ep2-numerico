360259027a30ef8e08add40187e4cf1996983a5d
marianaserrao <mserraoguilherme@gmail.com>
2021-07-12 15:38:59 -0300

    finished householder transformation

--- ep1.py
+++ ep1.py
@@ -2,6 +2,7 @@ import numpy as np
 
 A = np.array([[2,-1,1,3],[-1,1,4,2],[1,4,2,-1],[3,2,-1,1]]).astype(np.float32)
 n = np.size(A,0)
+HT = np.identity(n)
 
 def get_vector(A, index, column=False):
     if column:
@@ -21,7 +22,14 @@ def norm(x):
         value+=xi**2
     return value**(1/2)
 
-def get_Hx(x, w):        
+def get_wk(A):
+    ak = A[1:, 0:1].copy()
+    sign = np.sign(ak[0])
+    ak[0] = ak[0] + sign*norm(ak)
+    wk=[0, *ak]
+    return wk
+
+def get_Hx(x, w):
     xw = scalar_product(x,w)
     ww = scalar_product(w,w)
 
@@ -32,26 +40,43 @@ def get_Hx(x, w):
     hx = np.subtract(x,sub)
     return hx
 
-def get_w(k):
-    ai = A[k+1:n+1, k:k+1].copy()
-    sign = np.sign(ai[0])
-    ai[0] = ai[0] + sign*norm(ai)
-    w=[0, *ai]
-    return w
 
-def get_HAH():
-    k=0
-    HA = np.zeros([n,n])
-    w= get_w(k)
-    for i in range(n):
-        x = A[:,i:i+1]
-        HA[:,i:i+1]=get_Hx(x,w)
-    print(HA)
-    HAH = np.zeros([n,n])
-    for i in range(n):
-        x = HA[i:i+1,:]
+def get_transformation(M):
+
+    def right_transformation(M,result,i):
+        x = M[i:i+1,:]
         x = x.T
-        HAH[:,i:i+1]=get_Hx(x,w)
-    return HAH
+        result[:,i:i+1]=get_Hx(x,w)
 
-T= np.zeros([n,n])
+    m = np.size(M, 0)
+    w= get_wk(M)
+
+    HM = np.zeros([m,m])
+
+    for i in range(m):
+        x = M[:,i:i+1]
+        HM[:,i:i+1]=get_Hx(x,w)
+
+    HMH = np.zeros([m,m])
+    for i in range(m):
+        right_transformation(HM,HMH,i)
+        right_transformation(HT[n-m:n,n-m:n], HT[n-m:n,n-m:n], i)
+        
+        # x = HM[i:i+1,:]
+        # x = x.T
+        # HMH[:,i:i+1]=get_Hx(x,w)
+
+    return HMH
+
+def get_T():
+    T = np.zeros([n,n])
+    for i in range(n-2):
+        M = A[i:n+1,i:n+1]
+        M=get_transformation(M)
+        T[i:n+1,i:n+1] = M
+    return T
+
+T = get_T()
+
+print(T)
+print(HT)

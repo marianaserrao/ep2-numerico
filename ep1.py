19a4d857130e2263fd384206cb3e6d334edbb590
marianaserrao <mserraoguilherme@gmail.com>
2021-07-12 16:36:36 -0300

    commented

--- ep1.py
+++ ep1.py
@@ -2,26 +2,29 @@ import numpy as np
 
 A = np.array([[2,-1,1,3],[-1,1,4,2],[1,4,2,-1],[3,2,-1,1]]).astype(np.float32)
 n = np.size(A,0)
-HT = np.identity(n)
 
+#funcao para obter uma linha ou coluna de matriz
 def get_vector(A, index, column=False):
     if column:
         return A[:,index:index+1]
     else:
         return A[index:index+1,:]
 
+#funcao para obter o produto escalar de dois vetores
 def scalar_product(x,y):
     value = 0
     for i, xi in enumerate(x):
         value+=xi*y[i]
     return value
 
+#funcao para obter a norma de um vetor
 def norm(x):
     value = 0
     for i,xi in enumerate(x):
         value+=xi**2
     return value**(1/2)
 
+#funcao para obter o vetor w para transformacao Householder
 def get_wk(M):
     ak = M[1:, 0:1].copy()
     sign = np.sign(ak[0])
@@ -29,6 +32,7 @@ def get_wk(M):
     wk=[0, *ak]
     return wk
 
+#funcao para obter o produto de um vetor pela matriz Householder
 def get_Hx(x, w):
     xw = scalar_product(x,w)
     ww = scalar_product(w,w)
@@ -40,42 +44,51 @@ def get_Hx(x, w):
     hx = np.subtract(x,sub)
     return hx
 
+#funcao para obter um trasformacao Householder M->HMH
+def get_transformation(M, HT):
 
-def get_transformation(M):
+    def left_transformation(M,result,i):
+        x = M[:,i:i+1]
+        result[:,i:i+1]=get_Hx(x,w)
 
+    #funcao para obter cada coluna da matriz resultante da transformacao Householder pela direita
     def right_transformation(M,result,i):
         x = M[i:i+1,:]
         x = x.T
         result[:,i:i+1]=get_Hx(x,w)
 
+    #variaveis para a trasformacao
     m = np.size(M, 0)
     w= get_wk(M)
 
+    #variavel onde sera armazenado o resultado do produto da matriz Householder pela esquerda
     HM = np.zeros([m,m])
 
+    #iteracao para preenchimento de cada coluna da matriz HM
     for i in range(m):
-        x = M[:,i:i+1]
-        HM[:,i:i+1]=get_Hx(x,w)
+        left_transformation(M, HM, i)
 
+    #variavel onde sera armazenado o resultado do produto da matriz Householder pela direita
     HMH = np.zeros([m,m])
+    
+    #iteracao para preencimento de cada coluna das matrizes HMH e HT
     for i in range(m):
         right_transformation(HM,HMH,i)
         right_transformation(HT[n-m:n,n-m:n], HT[n-m:n,n-m:n], i)
-        
-        # x = HM[i:i+1,:]
-        # x = x.T
-        # HMH[:,i:i+1]=get_Hx(x,w)
+
     return HMH
+
 
 def get_T():
     T = A.copy()
+    HT = np.identity(n)
     for i in range(n-2):
         M = T[i:n+1,i:n+1]
-        M=get_transformation(M)
+        M=get_transformation(M, HT)
         T[i:n+1,i:n+1] = M
-    return T
+    return T, HT
 
-T = get_T()
+T, HT = get_T()
 
 print(T)
 print(HT)
